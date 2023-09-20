fe423de3191bbe4fc576175c72b1cf8724c32e4e
KamikazeFelix <unchinoiszyh@gmail.com>
2023-09-20 12:44:56 -0600

    update
    au

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import Student from './StudentInfo';
-import Page from './week2/page';
+
 
 export default function Home() {
   return (
@@ -8,7 +8,7 @@ export default function Home() {
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm">
       <h1 className="text-4xl font-bold mb-5">CPRG 306: Web Development 2 - Assignments</h1>
       <Student />
-      <Link href = "Week2">Shopping List</Link>
+      <Link href = "week2">Shopping List</Link>
       </div>
     </main>
   );
